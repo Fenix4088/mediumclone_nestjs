6ce69ccf45853a65c3e84e54513a297439b8279c
Fenix4088 <fenix4088@gmail.com>
2023-05-29 08:12:16 +0300

    generate migration

---
+++ src/migrations/1685276703480-CreateTags.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTags1685276703480 implements MigrationInterface {
+  name = 'CreateTags1685276703480';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "tags" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_e7dc17249a1148a1970748eda99" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "tags"`);
+  }
+}
